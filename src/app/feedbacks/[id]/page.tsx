1cbecddb9bca2d46b7829dc1008d16d103500dd6
West-tm <zerobnwn@gamil.com>
2026-06-10 13:16:48 +0900

    feat: feedback 詳細ページを実装

---
+++ src/app/feedbacks/[id]/page.tsx
@@ -0,0 +1,38 @@
+import { prisma } from "@/lib/prisma/prisma";
+import { createClient } from "@/lib/supabase/server";
+import { notFound, redirect } from "next/navigation";
+
+export default async function FeedbackPage(
+  Props: PageProps<"/feedbacks/[id]">,
+) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) redirect("/auth/signin");
+
+  const { id } = await Props.params;
+  const feedback = await prisma.feedback.findUnique({
+    where: { id, userId: user.id },
+  });
+
+  if (!feedback) {
+    notFound();
+  }
+
+  return (
+    <>
+      <div>フィードバック詳細</div>
+      <br />
+
+      <div>フィードバック：{feedback.content}</div>
+      <div>
+        作成日時：
+        {feedback.createdAt.toLocaleString("ja-JP", {
+          timeZone: "Asia/Tokyo",
+        })}
+      </div>
+    </>
+  );
+}
