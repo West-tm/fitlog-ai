b78320c8170a3b9eef9f3617c58f60d80b839fd3
West-tm <zerobnwn@gmail.com>
2026-08-06 17:16:41 +0900

    chore: 長い className を折り返す Prettier プラグインを追加

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import("prettier").Config & import("prettier-plugin-tailwindcss").PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: [
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-classnames",
+    "prettier-plugin-merge",
+  ],
   tailwindStylesheet: "./src/app/globals.css",
+  printWidth: 80,
 };
 
 export default config;
